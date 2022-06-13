3e66867b3eae50b0d858d177de3998bcc5390e0f
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-06-14 02:47:06 +0530

    play command -2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice")
 const { Client, Intents } = require("discord.js")
 const ytdl = require("ytdl-core")
+const getMusic = require("./src/getMusic")
 const fs = require("fs")
 const client = new Client({
     intents: [
@@ -13,9 +14,11 @@ const client = new Client({
 const defaultPrefix = "~"
 require("dotenv").config()
 const TOKEN = process.env.DISCORD_TOKEN
+const MUSIC_TOKEN = process.env.YOUTUBE_KEY
+
 
 function isMsgForBot(msg) {
-    if (!msg.content.startsWith(defaultPrefix)) return false
+    if (!msg.content.startsWith(defaultPrefix) || msg.author.bot) return false
     let msgArray = msg.content.split(" ")
     if (msgArray[0] === "~") return false
     return true
@@ -27,22 +30,39 @@ client.on("ready", () => {
 
 client.on("messageCreate", async (msg) => {
     if (isMsgForBot(msg)) {
-        let command = msg.content.slice(1)
-        if (command === "ping")
+        const args = msg.content.slice(defaultPrefix.length).trim().split(' ');
+        const command = args.shift().toLowerCase();
+        if (command === "ping" && args.length === 0)
             await msg.reply("pong!")
-        if (command === "play") {
-            let url = "https://www.youtube.com/watch?v=V54CEElTF_U"
-            const connection = await joinVoiceChannel({
-                channelId: msg.member.voice.channel.id,
-                guildId: msg.member.guild.id,
-                adapterCreator: msg.member.guild.voiceAdapterCreator
-            })
-            let newPlayer = createAudioPlayer()
-            let resource = createAudioResource(ytdl(url, {
-                filter: "audioonly"
-            }))
-            await newPlayer.play(resource)
-            await connection.subscribe(newPlayer)
+        else if (command === "play") {
+            if (args.length === 0) {
+                await msg.channel.send("Please pass the arguments")
+                return
+            }
+            let nameByUser = args.join(" ")
+            try {
+                let songData = await getMusic(nameByUser, MUSIC_TOKEN)
+                let url = `https://www.youtube.com/watch?v=${songData.id}`
+                const connection = await joinVoiceChannel({
+                    channelId: msg.member.voice.channel.id,
+                    guildId: msg.member.guild.id,
+                    adapterCreator: msg.member.guild.voiceAdapterCreator
+                })
+                let newPlayer = createAudioPlayer()
+                let resource = createAudioResource(ytdl(url, {
+                    filter: "audioonly"
+                }))
+                await newPlayer.play(resource)
+                await connection.subscribe(newPlayer)
+            }
+            catch (e) {
+                console.log("error in recieving data")
+                console.log(e)
+            }
+        }
+        else {
+            let textChannel = msg.channel
+            await textChannel.send("Unknown command")
         }
     }
 })
