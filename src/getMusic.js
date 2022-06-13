d18b464d52a4b059f7e465e847f240bff72ca709
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-06-14 02:45:36 +0530

    play command -2

---
+++ src/getMusic.js
@@ -0,0 +1,30 @@
+// require("dotenv").config({ path: __dirname + '../.env' })
+// const MUSIC_TOKEN = process.env.YOUTUBE_KEY
+const axios = require("axios")
+
+let getMusic = async (songName, MUSIC_TOKEN) => {
+    let songData = {}
+    let parameters = {
+        part: "snippet",
+        q: songName + " audio",
+        maxResults: 1,
+        key: MUSIC_TOKEN
+    }
+    try {
+        let searchSong = await axios.get(`https://www.googleapis.com/youtube/v3/search`, { params: parameters })
+        let songInfo = searchSong.data.items[0]
+        songData = {
+            id: songInfo.id.videoId,
+            title: songInfo.snippet.title,
+            authorChannel: songInfo.snippet.channelTitle,
+            source: "YouTube"
+        }
+    }
+    catch (e) {
+        console.log("error in requesting data")
+        console.log(e)
+    }
+    return songData
+}
+
+module.exports = getMusic
