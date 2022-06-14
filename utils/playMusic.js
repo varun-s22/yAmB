d83265047154a2efaaa0bd38079dbdda9981e2eb
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-06-14 23:22:05 +0530

    wrapped with try-catch

--- utils/playMusic.js
+++ utils/playMusic.js
@@ -4,17 +4,33 @@ const getMusic = require("../src/getMusic")
 
 async function playMusic(msg, newPlayer, nameByUser, MUSIC_TOKEN) {
     let songData = await getMusic(nameByUser, MUSIC_TOKEN)
+    if (!songData) {
+        console.log("can't play song")
+        return false
+    }
     let url = `https://www.youtube.com/watch?v=${songData.id}`
     const connection = await joinVoiceChannel({
         channelId: msg.member.voice.channel.id,
         guildId: msg.member.guild.id,
         adapterCreator: msg.member.guild.voiceAdapterCreator
     })
-    let resource = createAudioResource(ytdl(url, {
-        filter: "audioonly"
-    }))
-    await newPlayer.play(resource)
-    await connection.subscribe(newPlayer)
+    try {
+        let resource = createAudioResource(ytdl(url, {
+            filter: "audioonly"
+        }))
+        try {
+            await newPlayer.play(resource)
+            await connection.subscribe(newPlayer)
+        }
+        catch (e) {
+            console.log("error playing song")
+            console.log(e)
+        }
+    }
+    catch (e) {
+        console.log("error while downloading song")
+        console.log(e)
+    }
     return songData
 }
 
